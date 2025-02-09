fbc66fcf634e4294579542ba5721857070173786
Robin Salih <robin.salih@gmail.com>
2025-02-09 17:49:53 +0000

    Rearrange directory structure

---
+++ Tycoon.Factory.Tests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using Tycoon.Factory.Infrastructure;
+global using Tycoon.Factory.Core.Interfaces;
+global using Tycoon.Factory.Core.Model;
+global using Tycoon.Factory.Core.Scheduling;
+
+global using System.Collections.Immutable;
+
+global using Moq;
+global using Xunit;
